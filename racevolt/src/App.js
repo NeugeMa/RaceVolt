57fc1cd42b509f6b9f0610b95d642fa65cafe706
NeugeMa <mariana.n.dourado@gmail.com>
2024-10-24 21:10:23 -0300

    Iniciando página contato :)

--- racevolt/src/App.js
+++ racevolt/src/App.js
@@ -2,12 +2,16 @@ import './App.css'; // CSS Global
 import Cabecalho from './Components/Cabecalho/Cabecalho';
 import Rodape from './Components/Rodape/Rodape';
 
+import Contato from './pages/Contato/Contato';
+
 
 function App() {
   return (
     <div className="App">
       <Cabecalho />
 
+          <Contato />
+          
       <Rodape />
     </div>
   );
