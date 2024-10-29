e60b86edae6423ef1fd15186a8815e834e8cb703
NeugeMa <mariana.n.dourado@gmail.com>
2024-10-28 21:01:31 -0300

    Estilizando rodapé e adicionando formulário de inscrição

--- racevolt/src/Components/Rodape/Rodape.jsx
+++ racevolt/src/Components/Rodape/Rodape.jsx
@@ -1,33 +1,55 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { Link } from "react-router-dom";
+import logo from '../../assets/Logo-Ficaps.png';  
 // Adicionar LOGO + Arrumar formulário
 
-function Rodape() {
-    return (
-        <div className="container-footer">  
-        
-            <div className="content-footer">
-                <nav>
-                    <ul className="conteudo">
-                        <h4>PROJETO</h4>
-                        <li>Sobre Nós</li>
-                        <li>RaceVolt</li>
-                        <li>Contato</li>
-                    </ul>
-                </nav>
 
-                <nav>
-                    <ul className="conteudo">
-                        <h4>CONECTE</h4>
-                        <li>Github</li>
-                        <li>Linkdien</li>
-                    </ul>
-                </nav>
-            
-                <div className="forms-footer">
-                    <h2>Inscreva-se para receber nossas novidades!</h2>
-                    <input type="text" /> <button>EMAIL:</button>
-                </div>
+const Rodape = () => {
+    const handleSubmit = () => {
+        alert('E-mail enviado com sucesso!')
+        document.querySelector('.form-input').value = '';
+    }       
+
+    return (
+        <Router>
+            <div className="container-footer">  
+                <img src={logo} alt="Logo" className="logo" />
+                    <div className="content-footer">
+                        <nav>
+                            <ul className="conteudo">
+                                <h4>PROJETO</h4>
+                                    <li> <Link to="/sobre" className="url">Sobre Nós</Link> </li> 
+                                    <li> <Link to="/" className="url">RaceVolt</Link> </li> 
+                                    <li> <Link to="/contato" className="url">Contato</Link> </li> 
+                            </ul>
+                        </nav>
+
+                        <nav>
+                            <ul className="conteudo">
+                                <h4>CONECTE</h4>
+                                <li> <Link to="https://github.comhttps://github.com/NeugeMa/RaceVolt" className="url">GitHub</Link> </li> 
+                                <li> <Link to="https://linkedin.com" className="url">LinkedIn</Link> </li> 
+                            </ul>
+                        </nav>
+
+                        <nav>
+                            <ul className="conteudo">
+                                <h4>MAIS+</h4>
+                                    <li> <Link to="/" className="url">Premiações</Link> </li> 
+                                    <li> <Link to="/" className="url">Doações</Link> </li> 
+                            </ul>
+                        </nav>
+                    
+                        <div className="forms-footer">
+                            <h2>Inscreva-se para receber nossas novidades!</h2>
+                            <div className="form-container">
+                                <input type="text" className="form-input" /> 
+                                <button className="form-button" onClick={handleSubmit}>EMAIL:</button>
+                            </div>
+                        </div>
+                    </div>
             </div>
-        </div>
+        </Router>
     )
 }
 
