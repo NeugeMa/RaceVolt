a02ce2a1d95572abc3a0f6f111aff132104b2d8b
NeugeMa <mariana.n.dourado@gmail.com>
2024-10-29 22:24:42 -0300

    Estilizando título e subtitulo da página Home

--- racevolt/src/pages/Home.jsx
+++ racevolt/src/pages/Home.jsx
@@ -18,31 +18,27 @@ const Home = () => {
       <section className="introducao">
         <div className="introducao-fundo">
           <div className="intro-conteudo">
-          <h4 className="subtitulo"><span className="linha-subtitulo"></span>FUTURO VELOCISTAS</h4>
-              <h1 className="titulo">Experiência Inovadora para Crianças e Jovens</h1>
+          <h4 className="subtitulo01"><span className="linha-subtitulo01"></span>FUTURO VELOCISTAS</h4>
+              <h1 className="titulo01">Experiência Inovadora para Crianças e Jovens</h1>
               <p> Estamos em um momento crucial de transformação e precisamos ajustar nosso projeto para torná-lo mais escalável e relevante. A ideia central gira em torno da criação de uma plataforma que combina entretenimento e educação, permitindo que crianças e jovens explorem o fascinante mundo da engenharia, programação e design por meio de competições interativas com carros elétricos customizáveis. </p>
           </div>
           <img src={criancas} alt="Logo" className="intro-fundo" />
         </div>
       </section>
 
-      <main className="arduino">
-        <div className="arduino-fundo">
-          <div className="conteudo-arduino">
-          <h4 className="subtitulo02"><span className="linha-subtitulo02"></span>POR QUE, ARDUINO?</h4>
-
-              <h1 className="titulo">Carrinhos Arduino, o simples o melhor!</h1>
-              <p>Por meio da ludicidade e criatividade, elas serão imersas no desafiador mundo dos esportitas e das equipe de corrida. <br />
-
-              As competições RaceVolt são divididas em diferentes categorias, com base em nível de habilidade e objetivos dos participantes. Os participantes têm a liberdade de projetar, construir e programar seus veículos, ajustando variáveis como velocidade, eficiência energética, e estabilidade</p>
-
-              </div>
-              <img src={arduino} alt="Logo" className="intro-fundo" />
-          </div>
-      </main>
 
 
+      <section className="eventos">
+            <h4 className="subtitulo"><span className="linha-subtitulo"></span><span className="destaque">EVENTOS</span> - PREMIAÇÕES - FUTURO </h4>
+            <h1 className="titulo">EVENTOS DE GAMIFICAÇÃO</h1>
+                <div className="card-circulo">
+                      <div class="circulo"></div>
+                      <div class="circulo"></div>
+                      <div class="circulo"></div>
+                </div>
+      </section>
 
+      
 
     </div>
   );
