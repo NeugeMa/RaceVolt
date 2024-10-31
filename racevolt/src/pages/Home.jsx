1d48793aeefad3dfeb486b24cd9ded23d55009ba
NeugeMa <mariana.n.dourado@gmail.com>
2024-10-31 08:42:34 -0300

    Adicionando sessão anúncio + fórmulario

--- racevolt/src/pages/Home.jsx
+++ racevolt/src/pages/Home.jsx
@@ -5,7 +5,20 @@ import fundo  from '../assets/RaceVolt-Banner.png';
 import criancas from '../assets/image/criancas.jpg';
 import arduino from '../assets/image/Arduino.jpg';
 
+/* Imagens & Icons */ 
+import stand from '../assets/image/icons/icon-stand.png';
+import pista from '../assets/image/icons/icon-carrinho.png';
+import premio from '../assets/image/icons/icon-premio.png';
+import anuncio from '../assets/Banner-Anuncio.png'
+
+
+
 const Home = () => {
+  const handleSubmit = () => {
+    alert('E-mail enviado com sucesso!')
+    document.querySelector('.form-input').value = '';
+} 
+
   return (
     <div className="container-home">
       <img src={fundo} alt="Logo" className="fundo" />
@@ -26,18 +39,43 @@ const Home = () => {
         </div>
       </section>
 
-
-
       <section className="eventos">
-            <h4 className="subtitulo"><span className="linha-subtitulo"></span><span className="destaque">EVENTOS</span> - PREMIAÇÕES - FUTURO </h4>
-            <h1 className="titulo">EVENTOS DE GAMIFICAÇÃO</h1>
-                <div className="card-circulo">
-                      <div class="circulo"></div>
-                      <div class="circulo"></div>
-                      <div class="circulo"></div>
-                </div>
+          <h4 className="sub01"><span className="linha-sub01"></span><span className="destaque">EVENTOS</span> - PREMIAÇÕES - FUTURO</h4>
+          <h1 className="tit01">EVENTOS DE GAMIFICAÇÃO</h1>
+
+          <div className="container-eventos">
+            <div className="conteudo-eventos">
+              <div className="circulo">
+                <img src={stand} alt="Stand" className='img-circulo'/>
+              </div>
+              <p className="msg-circulo">Stand em polos comerciais populares</p>
+            </div>
+
+            <div className="conteudo-eventos">
+              <div className="circulo">
+                <img src={pista} alt="Pista de Corrida" className='img-circulo'/>
+              </div>
+              <p className="msg-circulo">Personalização de Carrinhos</p>
+            </div>
+
+            <div className="conteudo-eventos">
+              <div className="circulo">
+                <img src={premio} alt="Stand" className='img-circulo'/>
+              </div>
+              <p className="msg-circulo">Competição concorrendo a prêmios</p>
+            </div>
+          </div> 
       </section>
 
+      <section className="anuncio">
+        <img src={anuncio} alt="Banner Anúncio-RaceVolt" className="anuncio-img"/>
+          <div className="conteudo-anuncio">
+              <h1>Faça seu cadastro para receber as úlimas NOVIDADES:</h1>
+              <input type="text" className="anuncio-input" /> 
+              <button className="anuncio-button" onClick={handleSubmit}>EMAIL:</button>
+                <span>ESTE E-MAIL DEVE SER DE SEU RESPONSÁVEL LEGAL</span>
+          </div>
+      </section>
       
 
     </div>
